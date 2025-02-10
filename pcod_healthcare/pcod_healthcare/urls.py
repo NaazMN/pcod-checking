53c73f0c703f1404f775f5c8e706fb6d93b739b0
Renjish-k-s <renjishksimon@gmail.com>
2025-02-10 21:49:41 +0530

    project started

--- pcod_healthcare/pcod_healthcare/urls.py
+++ pcod_healthcare/pcod_healthcare/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pcod_finder import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', views.print_hello,name="home"),
+
 ]
